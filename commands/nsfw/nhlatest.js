c30cdc0675133be447b395eca530f6a74778ef7c
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-10-25 01:45:34 +0700

    update nhlatest.js

--- commands/nsfw/nhlatest.js
+++ commands/nsfw/nhlatest.js
@@ -15,6 +15,6 @@ let { conn } = data
 	let latest = await nhentai.getLatest()
     let txt = latest.map(({ title, id, language }) => {
 	return `*${title}*\n• ID: ${id}\n• Language: ${language}\n• Link: https://cin.pw/v/${id}`}).join('\n' + '='.repeat(25) + '\n')
-	await conn.sendFile(from, latest[0].thumbnail, '', '*NHentai Latest*\n\n' + txt, m)
+	await conn.sendFile(m.chat, latest[0].thumbnail, '', '*NHentai Latest*\n\n' + txt, m)
   }
 }
