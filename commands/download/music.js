613931223a1073c9ea4fb6979ee018ee3bdcd7bf
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-10-24 22:04:33 +0700

    Add files via upload

---
+++ commands/download/music.js
@@ -0,0 +1,30 @@
+const fs = Ft.fs
+let yts = require('yt-search')
+let fetch = require('node-fetch')
+
+module.exports = {
+
+name: ["music"],
+
+type: ["download"],
+useLimit: true,
+description: "Download and searching music dri YouTube",
+utilisation: userbot.prefix + "music <query>",
+
+async execute(m) {
+
+ let { conn, text } = data
+
+if (!text) return m.reply('Mau Cari Apa?')
+let results = await yts(text)
+let result = results.all
+if (!result) throw 'Pencarian Anda Tidak di Temukan'
+let berhitung = 1
+let xixixi = `*「 YOUTUBE MUSIC 」*\n\n*Hasil Pencarian : ${text} *\n\n─────────────────\n\nKetik ${userbot.prefix}getmusic [ Angka ] untuk mengambil ID, Contoh : ${userbot.prefix}getmusic 2\n`
+
+for (let i = 0; i < result.length; i++) {
+ xixixi += `\n─────────────────\n\n*Urutan* : ${berhitung+i}\n*Title* : ${result[i].title}\n*Channel* : ${result[i].author.name}\n*Durasi* : ${result[i].timestamp}\n*Perintah download* : \n*${prefix}getmusic ${result[i].videoId}*\n`
+                    }
+conn.sendMessage(m.chat,await (await Ft.fetch(result[0].thumbnail)).buffer(),"imageMessage",{quoted:m, caption: xixixi, thumbnail:Buffer.alloc(0)})
+}
+}
