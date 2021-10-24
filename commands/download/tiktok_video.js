b2e67c835a55fabef91b4cdf4bf561764a1b9037
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-10-25 05:26:17 +0700

    Add files via upload

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -1,21 +1,22 @@
-const fs = Ft.fs
-const tiktok = require("../../Lib/scrape.js").tiktok
-
+const { tiktokmusic } = require("../../Lib/scrape.js")
 module.exports = {
-name: ["tiktok"],
+name: ["tiktok", "tt"],
 type: ["download"],
 description: "download video from tiktok with url",
 utilisation: userbot.prefix + "tiktok <link>",
 
 async execute(m) {
-let { args } = data
+let { text } = data
+let url = m.quoted.text || text;
+//if ( m.quoted.text) return m.reply('reply textnya')
+if (!url) return m.reply("please input url")
 try {
-if (!args[0]) return m.reply("url ?")
-conn.send2Button(m.chat, `Pilih Type Barikut`, userbot.packname,`No wm`, `tnowm ${[args[0]]}`, `Music` , `tmusic ${[args[0]]}`, {quoted: m})
-//download = await tiktok([args[0]])
-//conn.sendMessage(m.chat, await (await Ft.fetch(download.result[args[1]])).buffer(),"videoMessage",{quoted:m})
+let ttdata = await tiktokmusic(url)
+let teks = `*Nickname :* ${ttdata.meta.author.nickname}\n*Desc :* ${ttdata.meta.desc}\n*Duration* : ${ttdata.meta.video.duration}\n\n_Pilih Type Dibawah Ini_ ${m.mention}`
+conn.send2ButtonImg(m.chat, teks, await Ft.getBuffer(ttdata.meta.video.cover), userbot.packname, "MP3","tmusic "+url, "MP4", "tnowm "+url, {contextInfo:{"mentionedJid": conn.parseMention(teks)}})
 } catch (e) {
 m.reply(Ft.util.format(e))
 }
 }
+//Uh
 }
