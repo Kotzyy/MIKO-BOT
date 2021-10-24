1bec14ffc1bfe573410b5aa7a768976bbb9725c2
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-10-24 22:28:43 +0700

    update music.js

--- commands/download/music.js
+++ commands/download/music.js
@@ -25,6 +25,10 @@ let xixixi = `*「 YOUTUBE MUSIC 」*\n\n*Hasil Pencarian : ${text} *\n\n──�
 for (let i = 0; i < result.length; i++) {
  xixixi += `\n─────────────────\n\n*Urutan* : ${berhitung+i}\n*Title* : ${result[i].title}\n*Channel* : ${result[i].author.name}\n*Durasi* : ${result[i].timestamp}\n*Perintah download* : \n*${prefix}getmusic ${result[i].videoId}*\n`
                     }
+  xixixi += `\n\n`
+ for (let ii = 0; ii < result.length; ii++) {
+ xixixi += `(#)${result[ii].videoId}`
+                }
 conn.sendMessage(m.chat,await (await Ft.fetch(result[0].thumbnail)).buffer(),"imageMessage",{quoted:m, caption: xixixi, thumbnail:Buffer.alloc(0)})
 }
 }
