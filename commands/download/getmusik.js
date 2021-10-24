54115f02c082e61f5e86a8a9ec9fa96c4610fad5
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-10-24 22:25:18 +0700

    update getmusik.js

--- commands/download/getmusik.js
+++ commands/download/getmusik.js
@@ -19,7 +19,8 @@ async execute(m) {
 if (!m.quoted.caption) return m.reply ('tolong reply hasil data yang di kirim oleh bot')
 if (!args[0]) return m.reply("id or angka?")
 let hasilSplit = m.quoted.caption.split("(#)")[`${args[1]}`]
-let server = ("https://youtu.be/" + hasilSplit || servers[0]).toLowerCase()
+let hasil = "https://youtu.be/" + hasilSplit
+let server = (hasil || servers[0]).toLowerCase()
 
   let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
 
