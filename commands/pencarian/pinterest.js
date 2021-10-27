fcac2f0d012a85bc0508683b5ebda849aed0ac70
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-10-27 15:58:22 +0700

    update pinterest.js

--- commands/pencarian/pinterest.js
+++ commands/pencarian/pinterest.js
@@ -11,9 +11,9 @@ utilisation: userbot.prefix+"pinterest",
 async execute(m){
 let { conn, text } = data
 if (!text) return m.reply('where text?')
-data = await axios.get(`https://api.anesmlbb.studio/api/search/pinterest?query=${text}&apikey=KotzKey`)
-const pekok = ['1','2','3','4','5','6','7','8','9','10','11','12','13']
-const pin = pekok[Math.floor(Math.random() * (pekok.length))]
+axios.get(`https://api.anesmlbb.studio/api/search/pinterest?query=${text}&apikey=KotzKey`).then((fear) =>{
+          pekok = ['1','2','3','4','5','6','7','8','9','10','11','12','13']
+          pin = pekok[Math.floor(Math.random() * (pekok.length))]
 buttons = [
   {
     buttonId: `pinterest ${text}`,
@@ -21,12 +21,13 @@ buttons = [
       displayText: `Next✨`
     },
     type:1}]
-fs.writeFileSync(`./kotz.jpeg`, await (await Ft.fetch(data.data.result[`${pin}`])).buffer())
+fs.writeFileSync(`./kotz.jpeg`, await (await Ft.fetch(fear.data.result[`${pin}`])).buffer())
 imageMsg = ( await conn.prepareMessage(from, fs.readFileSync(`./kotz.jpeg`), 'imageMessage', {thumbnail: Buffer.alloc(0)})).message.imageMessage
 buttonsMessage = {footerText:'Regards By Kotz', imageMessage: imageMsg,
 contentText:`*Hasil Pencarian Dari : ${text}*`,buttons,headerType:4}
 prep = await conn.prepareMessageFromContent(from,{buttonsMessage},{})
 conn.relayWAMessage(prep)
 fs.unlinkSync(`./kotz.jpeg`)
+})
 }
 }
