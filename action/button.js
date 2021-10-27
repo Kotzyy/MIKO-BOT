b83cfc74923d2452025fb834c4f48c0dd52bd53a
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-10-27 14:34:45 +0700

    update button.js

--- action/button.js
+++ action/button.js
@@ -219,7 +219,7 @@ let asu = `total commands ${Object.keys(Events).length}\n\n`
 for (i in db.data) {
 asu += `-${i}: ${db.data[i]}\n`
 }
-m.reply(asu.trim())
+m.reply(asu.trim()[5])
 break;
 
    case "rules":
