990ed525c85e935e69aa3eb7a570a74c8eb4f11f
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-10-27 15:36:48 +0700

    update pinterest.js

--- commands/pencarian/pinterest.js
+++ commands/pencarian/pinterest.js
@@ -1,15 +1,31 @@
-let scrap = require("../../Lib/scrape")
+let axios = require('axios')
+let fetch = require('node-fetch')
 
 module.exports = {
 name: ["pinterest"],
 type: ["searching"],
 description: "search image from pinterest",
-utilisation: userbot.prefix+"jalantikus",
+utilisation: userbot.prefix+"pinterest",
 
 async execute(m){
 let { conn, text } = data
 if (!text) return m.reply('where text?')
-scrap.pinterest(text).then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"*PINTEREST*\n\npencarian dari: " + text, m)
-)
+data = await axios.get(`https://ogata-api.herokuapp.com/api/search/pinterest?query=${q}&apikey=${apikey}`)
+const pekok = ['1','2','3','4','5','6','7','8','9','10','11','12','13']
+const pin = pekok[Math.floor(Math.random() * (pekok.length))]
+buttons = [
+  {
+    buttonId: `pinterest ${text}`,
+    buttonText:{
+      displayText: `Next✨`
+    },
+    type:1}]
+Ft.fs.writeFileSync(`./${m.sender}.jpeg`, await (await Ft.fetch(data.data.result[`${pin}`])).buffer())
+imageMsg = ( await conn.prepareMessage(from, Ft.fs.readFileSync(`./${m.sender}.jpeg`), 'imageMessage', {thumbnail: Buffer.alloc(0)})).message.imageMessage
+buttonsMessage = {footerText:'Regards By Kotz', imageMessage: imageMsg,
+contentText:`*Hasil Pencarian Dari : ${text}*`,buttons,headerType:4}
+prep = await conn.prepareMessageFromContent(from,{buttonsMessage},{})
+conn.relayWAMessage(prep)
+Ft.fs.unlinkSync(`./${m.sender}.jpeg`)
 }
 }
